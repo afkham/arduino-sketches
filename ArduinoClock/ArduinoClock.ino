d874f1ce086ca6f5e788fdc085eda9f0471b61d0
Afkham Azeez <azeez@wso2.com>
2013-05-08 16:29:48 +0530

    Updated

--- ArduinoClock/ArduinoClock.ino
+++ ArduinoClock/ArduinoClock.ino
@@ -6,8 +6,95 @@
 #include <Bounce.h>
 #include "RTClib.h"
 
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
 #define NOTE_A7  3520
 #define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
 
 // Shift register pins
 int latchPin = 7;
@@ -79,10 +166,11 @@ boolean setupAlarm = false;
 boolean alarmOn = false;
 
 int noteIndex = 0;
-const int noteCount = 2;
-//int notes[noteCount] = {NOTE_F7,NOTE_FS7, NOTE_A7, NOTE_AS7};
+const int noteCount = 4;
 int notes[noteCount] = {
-  NOTE_A7, NOTE_AS7};
+  NOTE_F7,NOTE_FS7, NOTE_A7, NOTE_AS7};
+//int notes[noteCount] = {
+//  NOTE_A7, NOTE_AS7};
 
 void setupRTC () {
   Wire.begin();
@@ -113,6 +201,11 @@ boolean leftButtonPressed = false;
 boolean rightButtonPressed = false;
 boolean alarmControlButtonPressed = false;
 
+long blinkStartTime = -1;
+boolean displayOn = true;
+boolean displayAlarm = true;
+int alarmSwapTime = -1;
+
 void loop(){
   checkAlarmButtons();
 
@@ -134,11 +227,31 @@ void loop(){
     }
   } 
   else {
-    int alarmMin = EEPROM.read(0);
-    int alarmHour = EEPROM.read(1);
-    displayTime(alarmMin, alarmHour);
-    //TODO: Blink alarm display
+   if(displayAlarm){
+      showAlarm(); 
+    } 
+    else {
+      hideAlarm();
+    }
+    alarmCycle();
   }
+}
+
+void alarmCycle(){
+  if(millis() - alarmSwapTime >= 2000){
+     alarmSwapTime = millis();
+     displayAlarm = !displayAlarm;
+  }
+}
+
+void showAlarm(){
+  int alarmMin = EEPROM.read(0);
+  int alarmHour = EEPROM.read(1);
+  displayTime(alarmMin, alarmHour);
+}
+
+void hideAlarm(){
+  resetDigits();
 }
 
 void checkAlarmButtons(){
@@ -147,28 +260,33 @@ void checkAlarmButtons(){
 
   rightButton.update();
   int rightValue = rightButton.read();
-  
+
   alarmControlButton.update();
   int alarmControlValue = alarmControlButton.read();
-  
+
   if(alarmControlValue == HIGH){
     if(!alarmControlButtonPressed){
       alarmControlButtonPressed = true;
-       if(!setupAlarm){
-          setupAlarm = true;
-          Serial.println("setupAlarm = true");
-       } else {
-          setupAlarm = false; 
-          Serial.println("setupAlarm = false");
-       }
+      if(!setupAlarm){
+        setupAlarm = true;
+        alarmSwapTime = millis();
+        Serial.println("setupAlarm = true");
+      } 
+      else {
+        setupAlarm = false; 
+        alarmSwapTime = -1; 
+        Serial.println("setupAlarm = false");
+      }
     }
-  } else {
+  } 
+  else {
     alarmControlButtonPressed = false;
   }
-  
+
   if ( leftValue == HIGH) {
     if(!leftButtonPressed){
       Serial.println("Left button pressed");
+      //      alarmOn = true;
       leftButtonPressed = true;
       if(setupAlarm){
         alarmHour++;
@@ -186,6 +304,7 @@ void checkAlarmButtons(){
   if ( rightValue == HIGH) {
     if(!rightButtonPressed){
       Serial.println("Right button pressed");
+      //      alarmOn = true;
       rightButtonPressed = true;
       if(setupAlarm){
         alarmMin++;
@@ -228,38 +347,6 @@ void updateShiftRegister(byte value)
 
 long noteStartTime = -1;
 
-boolean offAlarm(){
-  alarmOn = false;
-  return alarmOn;
-}
-
-/*void checkAlarmSetup(){
-  if(leftButtonPressed && rightButtonPressed){
-    if(!setupAlarm){
-      setupAlarm = true; 
-    } 
-    else {
-      setupAlarm = false;   
-    }
-  } 
-  else if(setupAlarm){
-    if(rightButtonPressed){
-      alarmMin++;
-      if(alarmMin >= 60){
-        alarmMin = 0; 
-      }
-      EEPROM.write(0, alarmMin);
-    }
-    if(leftButtonPressed){
-      alarmHour++;
-      if(alarmHour >= 24){
-        alarmHour = 0;
-      }
-      EEPROM.write(1, alarmHour);
-    }
-  }
-}*/
-
 boolean soundAlarm(int minute,int  hour){
   byte _minute = EEPROM.read(0);
   byte _hour = EEPROM.read(1);
@@ -285,11 +372,15 @@ boolean playTone(int note, int noteDuration){
     noteStartTime = -1;
   } 
   else {
-    Serial.println(false);
     return false;
   }
   return true;
 }
+
+
+
+
+
 
 
 
